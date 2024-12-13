4d8eb4956fa1088f31084b587628b5fb789b01bc
Pierredelavigne <72016192+Pierredelavigne@users.noreply.github.com>
2024-12-13 16:54:54 +0100

    update code

--- Architecture-logicielle/micro-ynov-kafka/card-service/src/main/java/org/example/cardservice/service/impl/CardServiceImpl.java
+++ Architecture-logicielle/micro-ynov-kafka/card-service/src/main/java/org/example/cardservice/service/impl/CardServiceImpl.java
@@ -1,5 +1,6 @@
 package org.example.cardservice.service.impl;
 
+import jakarta.transaction.Transactional;
 import org.example.cardservice.entity.Card;
 import org.example.cardservice.repository.CardRepository;
 import org.example.cardservice.rest.AccountServiceClient;
@@ -9,6 +10,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.beans.Transient;
 import java.util.List;
 
 @Service
@@ -47,6 +49,7 @@ public class CardServiceImpl implements CardService {
     }
 
     @Override
+    @Transactional
     public void deleteCardByAccountId(Long accountId) {
         logger.info("Deleting card by account id {}", accountId);
         cardRepository.deleteByAccountId(accountId);
